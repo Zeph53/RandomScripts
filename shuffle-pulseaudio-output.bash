c1000edc6e097724178a4e73064c64e7a2d7b39a
Zeph53 <102870900+Zeph53@users.noreply.github.com>
2023-12-12 22:42:18 -0500

    Add files via upload

--- shuffle-pulseaudio-output.bash
+++ shuffle-pulseaudio-output.bash
@@ -24,3 +24,24 @@ echo $AVAILSINKIDS
 #for "0" in "$ACTIVEINDEXID" ; do
     
 #done
+
+sleep 3
+# Get the active process ID
+ACTIVEPROCID=$(xprop -id $(xprop -root -f _NET_ACTIVE_WINDOW 0x " \$0\\n" _NET_ACTIVE_WINDOW | awk '{print $2}') | awk -F '= ' '/NET_WM_PID/{print $2}')
+
+# Get the active index ID
+ACTIVEINDEXID=$(pacmd list-sink-inputs | awk -v pid="$ACTIVEPROCID" '$1 == "index:" {idx = $2} $1 == "application.process.id" && $3 == "\"" pid "\"" {print idx}')
+
+# Get available sink IDs
+AVAILSINKIDS=$( printf "$( pacmd list-cards | grep index | sed 's/.*: //g' | sed -z 's/\n/ /g' )")
+
+echo "Active Process ID: $ACTIVEPROCID"
+echo "Active Index ID: $ACTIVEINDEXID"
+echo "Available Sink ID: $AVAILSINKIDS"
+
+# Shuffle audio output
+if [ -n "$ACTIVEINDEXID" ] && [ -n "$AVAILSINKIDS" ]; then
+    pactl move-sink-input "$ACTIVEINDEXID" "$AVAILSINKIDS"
+else
+    echo "No active audio stream found for the current active window or available sink ID."
+fi
