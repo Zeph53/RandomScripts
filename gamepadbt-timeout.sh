0e75f30d8f9be4cadce7e3b03fa8d350f9d75c26
Zeph53 <102870900+Zeph53@users.noreply.github.com>
2025-04-26 16:58:20 -0400

    Update gamepadbt-timeout.sh

--- gamepadbt-timeout.sh
+++ gamepadbt-timeout.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+github forcing the use of 2fa now. i don't own a cellphone and i won't be buying one just to create free stuff.
+i won't be relying on third party password managers, or less-than-functional authentication apps either.
 
-if grep "PLAYSTATION(R)3" /sys/class/bluetooth/hci0:*/*/uevent > /dev/null ;
-then
-    sleep 0
-else
-    printf "failed"
-fi
+it was an interesting run on gitHub for like 7 years. 
+not a single comment, reaction, issue report, star, feedback of any kind..
+still tried sending things out there..
+
+if i can't access any of the stuff that i've made due to github's forcing of 2fa, then none of you can either.
