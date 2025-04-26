01beec7146176888aa56b9f64f9f4e3f609b075d
Zeph53 <102870900+Zeph53@users.noreply.github.com>
2025-04-26 16:55:30 -0400

    Update 0-LAUNCHER (copy 1).bash

--- Warband/0-LAUNCHER/0-LAUNCHER (copy 1).bash
+++ Warband/0-LAUNCHER/0-LAUNCHER (copy 1).bash
@@ -1,49 +1,8 @@
-#!/bin/bash
+github forcing the use of 2fa now. i don't own a cellphone and i won't be buying one just to create free stuff.
+i won't be relying on third party password managers, or less-than-functional authentication apps either.
 
-#
-##
-export DIR="$(dirname "$(readlink -f "$0")")" && pwd
+it was an interesting run on gitHub for like 7 years. 
+not a single comment, reaction, issue report, star, feedback of any kind..
+still tried sending things out there..
 
-MENU=\
-($(echo "$(grep '"$ANSWER" = ' $0 | grep -v 'MENU=')" | \
-awk -F ' ' '{print $5}' | sed -z 's/"//g')) ;
-
-ANSWER=\
-`zenity --list --column '' --hide-header --width 200 --height 250 "${MENU[@]}"` ;
-
-if [ "$ANSWER" = "Launch" ]; then
-    "$DIR/../mb_warband.sh" ;
-    $0 ;
-fi
-
-if [ "$ANSWER" = "Vanilla-Options" ]; then
-    "$DIR/../mbw_config.sh" ;
-    $0 ;
-fi
-
-
-
-
-
-if [ "$ANSWER" = "Options" ]; then
-    MBCONF="$HOME/.mbwarband/rgl_config.txt"
-    OPTMENU=($(echo "$(grep '"$OPTION" = ' $0 | grep -v 'OPTMENU=')" | awk -F ' ' '{print $5}' | sed -z 's/"//g')) ;
-    OPTION=`zenity --list --column '' --hide-header --width 200 --height 250 "${OPTMENU[@]}"` ;
-
-    if [ "$OPTION" = "Set-Display-Width" ]; then
-        DEFWIDTH="$(grep -Eo '^display_width = *[0-9]+' $MBCONF | grep -o '[0-9]*')"
-        RESWIDTH="$(zenity --scale --step 1 --value $DEFWIDTH --min-value 640 --max-value 2560)"
-        if [[ $? == 0 ]]; then
-            sed -i "s/^display_width = .*$/display_width = $RESWIDTH/" $MBCONF ;
-        fi
-        $0 ;
-    fi
-    if [ "$OPTION" = "Set-Display-Height" ]; then
-        DEFHEIGHT="$(grep -Eo '^display_height = *[0-9]+' $MBCONF | grep -o '[0-9]*')"
-        RESHEIGHT="$(zenity --scale --step 1 --value $DEFHEIGHT --min-value 640 --max-value 2560)"
-        if [[ $? == 0 ]]; then
-            sed -i "s/^display_height = .*$/display_height = $RESHEIGHT/" $MBCONF ;
-        fi
-        $0 ;
-    fi
-fi
+if i can't access any of the stuff that i've made due to github's forcing of 2fa, then none of you can either.
