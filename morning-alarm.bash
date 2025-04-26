df0e1e0a6840e15a85ef90b6cbc71bf03e03b251
Zeph53 <102870900+Zeph53@users.noreply.github.com>
2025-04-26 17:00:17 -0400

    Update morning-alarm.bash

--- morning-alarm.bash
+++ morning-alarm.bash
@@ -1,18 +1,8 @@
-#!/bin/bash
+github forcing the use of 2fa now. i don't own a cellphone and i won't be buying one just to create free stuff.
+i won't be relying on third party password managers, or less-than-functional authentication apps either.
 
-#
-## 
+it was an interesting run on gitHub for like 7 years. 
+not a single comment, reaction, issue report, star, feedback of any kind..
+still tried sending things out there..
 
-pkill quodlibet ;\
-sleep 3 ;\
-pactl set-sink-volume @DEFAULT_SINK@ 32768 ;\
-quodlibet \
-  --run \
-  --volume 100 \
-  --repeat 1 \
-  --repeat-type current \
-  --start-playing \
-  --play \
-  --add-location "/media/Multimedia-Z/Hassium.ogg" \
-  --play-file "/media/Multimedia-Z/Hassium.ogg" \
-  --enqueue "/media/Multimedia-Z/Hassium.ogg" 
+if i can't access any of the stuff that i've made due to github's forcing of 2fa, then none of you can either.
