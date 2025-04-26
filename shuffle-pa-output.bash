df468183455ac9d648007603c0370f75283fa0d2
Zeph53 <102870900+Zeph53@users.noreply.github.com>
2025-04-26 17:00:48 -0400

    Update shuffle-pa-output.bash

--- shuffle-pa-output.bash
+++ shuffle-pa-output.bash
@@ -1,56 +1,8 @@
-#!/bin/bash
+github forcing the use of 2fa now. i don't own a cellphone and i won't be buying one just to create free stuff.
+i won't be relying on third party password managers, or less-than-functional authentication apps either.
 
-#
-##
-#
-#
-#
-## Launch options
-if
-  [[ "$1" == "--help" ]] ||
-  [[ "$1" == "-h" ]]
-then
-  printf "
-Usage: 
+it was an interesting run on gitHub for like 7 years. 
+not a single comment, reaction, issue report, star, feedback of any kind..
+still tried sending things out there..
 
-$(basename "$0") [OPTION]
-
-Switch the audio output sink associated with the current active 
-window process to the next available audio output sink.
-
-Options:
---next, -n        Switch to the next sink when an audio window becomes active.
---current, -c     Switch the audio output sink associated with the current process to the next available sink.
---help, -h        Display this help menu.
-
-
-
-
-PulseAudio-ActiveWindowOutput-Shuffler
-Copyright (C) 2024 GitHub.com/Zeph53
-This program comes with ABSOLUTELY NO WARRANTY!
-This is free software, and you are welcome to
-redistribute it under certain conditions.
-See \"https://www.gnu.org/licenses/gpl-3.0.txt\"
-"
-#
-#
-#
-#
-## 
-active_pid="$(xprop -id $(xprop -root "_NET_ACTIVE_WINDOW" | cut -d ' ' -f 5) "_NET_WM_PID" | cut -d ' ' -f 3)"
-printf "$active_pid\n"
-
-
-##
-get_active_index() {
-    local pid="$1"
-    pacmd list-sink-inputs | awk -v pid="$pid" '
-        $1 == "index:" { idx = $2; app_id = 0 }
-        $1 == "application.process.id" && $3 == "\""pid"\"" {
-            print idx
-            exit
-        }
-    '
-}
-
+if i can't access any of the stuff that i've made due to github's forcing of 2fa, then none of you can either.
