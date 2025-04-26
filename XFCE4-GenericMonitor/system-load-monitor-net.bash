6d41f13eb983c1e20e8f8cd9d88d00f356570942
Zeph53 <102870900+Zeph53@users.noreply.github.com>
2025-04-26 16:56:48 -0400

    Update system-load-monitor-net.bash

--- XFCE4-GenericMonitor/system-load-monitor-net.bash
+++ XFCE4-GenericMonitor/system-load-monitor-net.bash
@@ -1,30 +1,8 @@
-#!/bin/bash
+github forcing the use of 2fa now. i don't own a cellphone and i won't be buying one just to create free stuff.
+i won't be relying on third party password managers, or less-than-functional authentication apps either.
 
-# Fixed filename for the temporary file to store network traffic readings
-tempfile="$HOME/.cache/network_traffic.tmp"
+it was an interesting run on gitHub for like 7 years. 
+not a single comment, reaction, issue report, star, feedback of any kind..
+still tried sending things out there..
 
-# Interface name
-interface="$(printf "$(ifconfig | grep RUNNING | grep -v lo | awk -F ":" '{print $1}')\n")"
-
-# Create temporary file if it doesn't exist
-touch "$tempfile"
-
-# Read the last entry from the temporary file
-read last_recv last_sent last_timestamp < "$tempfile" 2>/dev/null
-
-# Get the current readings
-current_recv=$(grep "$interface:" /proc/net/dev | awk '{print $2}')
-current_sent=$(grep "$interface:" /proc/net/dev | awk '{print $10}')
-
-# Update the temporary file with the current readings
-echo "$current_recv $current_sent $(date '+%s')" > "$tempfile"
-
-# Calculate the increase in traffic in Mbps
-if [[ -n "$last_recv" && -n "$last_sent" ]]; then
-    # Calculate the increase in received traffic in Mbps
-    increase_recv=$(echo "scale=2; ($current_recv - $last_recv) * 8 / 1024 / 1024" | bc)
-    # Calculate the increase in sent traffic in Mbps
-    increase_sent=$(echo "scale=2; ($current_sent - $last_sent) * 8 / 1024 / 1024" | bc)
-    printf "%07.3fMbps\n" "$increase_recv"
-    printf "%07.3fMbps\n" "$increase_sent"
-fi
+if i can't access any of the stuff that i've made due to github's forcing of 2fa, then none of you can either.
