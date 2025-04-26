994792287a8ce68b7bfd11a1b7f92a977ec9e10d
Zeph53 <102870900+Zeph53@users.noreply.github.com>
2025-04-26 16:55:43 -0400

    Update 0-LAUNCHER.bash

--- Warband/0-LAUNCHER/0-LAUNCHER.bash
+++ Warband/0-LAUNCHER/0-LAUNCHER.bash
@@ -1,43 +1,8 @@
-#!/bin/bash
+github forcing the use of 2fa now. i don't own a cellphone and i won't be buying one just to create free stuff.
+i won't be relying on third party password managers, or less-than-functional authentication apps either.
 
-#
-##
+it was an interesting run on gitHub for like 7 years. 
+not a single comment, reaction, issue report, star, feedback of any kind..
+still tried sending things out there..
 
-pdir="$(dirname "$(dirname "$0")")"
-menu="$(echo "$(grep '"$answer" = ' "$0" | grep -v 'menu=')" | awk -F ' ' '{print $5}' | sed -z 's/"//g')"
-answer="`zenity --list --column '' --hide-header --width 250 --height 500 "${menu[@]}"`"
-
-if [[ "$answer" == "Launch" ]]
-then
-    "$pdir/mb_warband.sh"
-    "$0"
-fi
-
-if [[ "$answer" == "Vanilla-Options" ]]
-then
-    "$pdir/mbw_config.sh"
-    "$0"
-fi
-
-#if [ "$answer" = "Options" ]
-#then
-#    mbconf="$HOME/.mbwarband/rgl_config.txt"
-#    menuentries="$(awk -F ' ' '/./{print $1 " " $3}' "$mbconf" | sort)"
-#    option=`zenity --list --separator " = " --column "Setting" --column "Value" --print-column "ALL" --width "500" --height "500" "$menuentries"`
-#    if [ "$option" ]
-#    then
-#        seloption="$(printf "$option" | awk -F ' ' '{print $1}')"
-#        seloptdef="$(printf "$menuentries" | grep "$seloption" | awk -F ' ' '{print $2}')"
-#        seloptval=`zenity --entry --text "$seloption" --entry-text "$seloptdef" --width "50" --height "50"`
-#        sed -i 's/'"$seloption"' = '"$seloptdef"'/'"$seloption"' = '"$seloptval"'/g' "$mbconf"
-#        "$0"
-#        echo "$seloption"
-#        echo "$seloptdef"
-#        echo "$seloptval"
-#    fi
-#fi
-
-if
-  [[ "$answer" == "Options" ]]
-then
-fi
+if i can't access any of the stuff that i've made due to github's forcing of 2fa, then none of you can either.
